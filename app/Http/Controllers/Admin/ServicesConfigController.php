f9e4c3305ecf9d46b3fd878f7f956a3addea80f6
praveenkanonia <142877886+praveenkanonia@users.noreply.github.com>
2023-09-02 19:39:51 +0530

    serviceSubCategory-add

--- app/Http/Controllers/Admin/ServicesConfigController.php
+++ app/Http/Controllers/Admin/ServicesConfigController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Admin;
 use App\Models\Admin\ServiceCategory;
+use App\Models\Admin\ServiceSubCategory;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Http\RedirectResponse;
